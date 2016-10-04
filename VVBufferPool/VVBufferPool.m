9bcf8cad0cefad151434ee686d12dbdfc30ee05e
Skye Book <skye.book@gmail.com>
2016-10-04 13:36:57 -0400

    Use nearest neighbor interpolation

--- VVBufferPool/VVBufferPool.m
+++ VVBufferPool/VVBufferPool.m
@@ -550,8 +550,10 @@ VVStopwatch		*_bufferTimestampMaker = nil;
 			glPixelStorei(GL_UNPACK_SWAP_BYTES, GL_FALSE);
 #endif	//	!TARGET_OS_IPHONE
 			
-			glTexParameteri(newBufferDesc.target, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-			glTexParameteri(newBufferDesc.target, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+            //glTexParameteri(newBufferDesc.target, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+            //glTexParameteri(newBufferDesc.target, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+			glTexParameteri(newBufferDesc.target, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+			glTexParameteri(newBufferDesc.target, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 			glTexParameteri(newBufferDesc.target, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 			glTexParameteri(newBufferDesc.target, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 			//glTexParameteri(newBufferDesc.target, GL_TEXTURE_WRAP_S, GL_REPEAT);
